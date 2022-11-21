3abf9e1907f6c572db78da2d49dcee2996dd8ebe
MariaLoraDAW <118777123+MariaLoraDAW@users.noreply.github.com>
2022-11-21 23:36:27 +0100

    Add files via upload

---
+++ fibonacciMain/src/main/java/ies/soterohernandez/daw/endes/fibonacciMain/fibonacciMain.java
@@ -0,0 +1,17 @@
+package ies.soterohernandez.daw.endes.fibonacciMain;
+import ies.soterohernandez.daw.endes.*;
+public class fibonacciMain {
+
+	public static void main(String[] args) {
+		 fibonacci f1 = new fibonacci("Fibonacci 1",10);
+		 
+		 System.out.println("HOLA AQUÍ TIENES A DOS FIBONACCIS");
+	       f1.mostrarSerie();
+	       
+	       fibonacci f2 = new fibonacci("Fibonacci",20);
+	       f2.mostrarSerie();
+	      
+
+	}
+
+}
